758e02a7bd1aec8e0aece9c4e71eb9638a5bacf7
Akbar <akhan2023@yahoo.com>
2019-06-26 17:32:17 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var ctx = c.getContext("2d");
 let jump = 0;
 let score = 0;
 let speed = 0;
+let pipesN = [];
+let pipesS = [];
 let checkScore = false;
 let bgX = 0;
 let bg = new Image();
@@ -15,8 +17,7 @@ var pipeSouth = new Image();
 pipeSouth.src = "images/pipeSouth.png";
 
 let fly = new Audio();
-fly.src = "sounds/score.mp3"
-
+fly.src = "sounds/score.mp3";
 
 
 // Class Bird
@@ -37,8 +38,10 @@ class Piller {
     }
 }
 
-let pipeN = new Piller(1000, -50); // init piller
-let pipeS = new Piller(1000, 350); // init piller
+
+pipesN.push( new Piller(1000, -200));
+pipesS.push(new Piller(1000, 200));
+
 
 // Contact function
 
@@ -62,7 +65,7 @@ document.addEventListener('keypress', function(){
 function draw(){
 
     // Score
-    if(bird.x > pipeS.x && !checkScore){
+    if(bird.x > pipesS[0].x && !checkScore){
         score++;
         fly.play();
         document.getElementById("points").innerText = score;
@@ -76,16 +79,14 @@ function draw(){
     if(bird.y < 0){bird.y = 0; speed = 0;}; /* top control */
 
     // North piller movings
-        pipeN.x -= 4;
-        if(pipeN.x+52 < 0){pipeN.x = 800
-        pipeN.y = Math.random()*200-230;
-        pipeS.y = pipeN.y+400;
+        if(pipesN[0].x+pipeNorth.width < 0){
+        pipesN.splice(0,1);
+        pipesS.splice(0,1);
         checkScore = false;
         };/* abs control */
 
         
         // South piller movings        
-        pipeS.x = pipeN.x; // pes besoin, se déplace par rapport à la barre du haut
         /* if(pipeS.x+52 < 0){pipeS.x = 800;}; *//* abs control pas besoin */
         
         // Background moving
@@ -94,7 +95,18 @@ function draw(){
         // Speed de descente
         if(speed < 7){speed+=.5}
         
+        while(pipesN[pipesN.length-1].x < c.width){
 
+            let randomY = Math.random()*200-230; // ordonnée aléatoire
+            let newX = pipesN[pipesN.length-1].x + 300; // new pipe par rapport au précédent
+
+            let pipeN = new Piller(newX, randomY); // new north piller
+            let pipeS = new Piller(newX, randomY+400); // new south piller
+
+
+            pipesN.push(pipeN)
+            pipesS.push(pipeS)
+        }
 
     ctx.clearRect(0, 0, 800, 500);
     ctx.fillRect(0, 0, 800, 500);
@@ -104,10 +116,17 @@ function draw(){
     ctx.drawImage(bg,bgX,0);
     ctx.drawImage(bg,bgX+bg.width,0);
     ctx.drawImage(oiseau, bird.x, bird.y);
-    ctx.drawImage(pipeNorth, pipeN.x, pipeN.y);
-    ctx.drawImage(pipeSouth, pipeS.x, pipeS.y); /* pipeN.x pour garder la symétrie par rapport à la barre du haut */
 
-    if(contactPiller(pipeS,pipeSouth) || contactPiller(pipeN, pipeNorth)){
+    for(let i = 0; i<pipesN.length; i++){
+        pipesS[i].x -= 4;
+        pipesN[i].x -= 4;
+
+        ctx.drawImage(pipeNorth, pipesN[i].x, pipesN[i].y);
+        ctx.drawImage(pipeSouth, pipesS[i].x, pipesS[i].y); /* pipeN.x pour garder la symétrie par rapport à la barre du haut */
+    }
+
+
+    if(contactPiller(pipesS[0],pipeSouth) || contactPiller(pipesN[0], pipeNorth)){
         if(confirm('Oops ! Piou-piou s\'est blessé !\nTon score : ' + score + '\nRéessayer ?')){
             location.reload();
         }
